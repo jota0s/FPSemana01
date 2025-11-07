e80466b04d5abc4b5c223bfe210bdda9a66491ae
Joao Silva <joaosilvaa.work@gmail.com>
2025-11-07 14:48:48 +0000

    add python file

--- character.py
+++ character.py
@@ -1,11 +1,37 @@
-name1 = input("")
+name1 = (str(input("")))
 Ataque1 = input ("")
 Defesa1 = input ("")
 
-name2 = input("")
+name2 = (str (input("")))
 Ataque2 = input ("")
 Defesa2 = input ("")
 
-name3 = input("")
+name3 =  (str (input("")))
 Ataque3 = input ("")
 Defesa3 = input ("")
+
+array = [[name1,(Ataque1, Defesa1)], [name2, (Ataque2, Defesa2)], [name2, (Ataque3, Defesa3)]]
+
+print (array)
+
+for i in array:
+    if Ataque1 > Ataque2
+    print("Ataque" + name1 + Ataque1) 
+
+    if Ataque1 > Ataque3
+    print("Ataque" + name1 + Ataque1)
+
+     if Ataque2 > Ataque1
+    print("Ataque" + name2 + Ataque2) 
+
+    if Ataque2 > Ataque3
+    print("Ataque" + name2 + Ataque2)
+
+     if Ataque3 > Ataque1
+    print("Ataque" + name3 + Ataque3) 
+
+    if Ataque3 > Ataque2
+    print("Ataque" + name3 + Ataque3)
+
+
+    
