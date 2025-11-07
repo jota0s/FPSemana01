4627770f71cd4d72abf2dd1cc535d71237cfa713
Joao Silva <joaosilvaa.work@gmail.com>
2025-11-07 17:29:13 +0000

    modify python filr

--- character.py
+++ character.py
@@ -1,37 +1,29 @@
-name1 = (str(input("")))
-Ataque1 = input ("")
-Defesa1 = input ("")
+name1 = (input(""))
+Ataque1 = int(input (""))
+Defesa1 = int(input (""))
 
-name2 = (str (input("")))
-Ataque2 = input ("")
-Defesa2 = input ("")
+name2 =  (input(""))
+Ataque2 = int(input (""))
+Defesa2 = int(input (""))
 
-name3 =  (str (input("")))
-Ataque3 = input ("")
-Defesa3 = input ("")
+name3 =  (input(""))
+Ataque3 = int(input (""))
+Defesa3 = int(input (""))
 
-array = [[name1,(Ataque1, Defesa1)], [name2, (Ataque2, Defesa2)], [name2, (Ataque3, Defesa3)]]
+array = [[name1,(Ataque1, Defesa1)], [name2, (Ataque2, Defesa2)], [name3, (Ataque3, Defesa3)]]
 
 print (array)
 
-for i in array:
-    if Ataque1 > Ataque2
-    print("Ataque" + name1 + Ataque1) 
+Maior_ataque = array[0]
+Maior_defesa = array[0]
 
-    if Ataque1 > Ataque3
-    print("Ataque" + name1 + Ataque1)
+for name, (Ataque, Defesa) in array :
+    if Ataque > Maior_ataque[1][0]:
+        Maior_ataque = [name, (Ataque, Defesa)]
 
-     if Ataque2 > Ataque1
-    print("Ataque" + name2 + Ataque2) 
+    if Defesa > Maior_defesa[1][1]:
+        Maior_defesa = [name, (Ataque, Defesa)]
 
-    if Ataque2 > Ataque3
-    print("Ataque" + name2 + Ataque2)
+print(f"Ataque {Maior_ataque[0]} {Maior_ataque[1][0]}")
+print(f"Defesa {Maior_defesa[0]} {Maior_defesa[1][1]}")
 
-     if Ataque3 > Ataque1
-    print("Ataque" + name3 + Ataque3) 
-
-    if Ataque3 > Ataque2
-    print("Ataque" + name3 + Ataque3)
-
-
-    
