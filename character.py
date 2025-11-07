80797cbd1ccfaeddb7ac5846d02ff84682495ead
Joao Silva <joaosilvaa.work@gmail.com>
2025-11-07 14:20:23 +0000

    add git ignore

--- character.py
+++ character.py
@@ -1,1 +1,11 @@
-g
+name1 = input("")
+Ataque1 = input ("")
+Defesa1 = input ("")
+
+name2 = input("")
+Ataque2 = input ("")
+Defesa2 = input ("")
+
+name3 = input("")
+Ataque3 = input ("")
+Defesa3 = input ("")
